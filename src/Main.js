964fb49c8c8d266a21e98987e5ccc3f4b51f1b84
Sun ju Lee <yohekisa@gmail.com>
2020-04-23 21:49:00 +0900

    modified tap nav -last v

--- src/Main.js
+++ src/Main.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react'
-import Nav from './components/Nav/Nav';
-import Model from './pages/Model/Model';
-import Hero from './pages/Hero/Hero';
-import Apply from './pages/Apply/Apply';
-import Footer from './pages/Footer/Footer';
-import TheWorldOfMaserati from './pages/TheWorldOfMaserati/TheWorldOfMaserati';
+import React, { Component } from "react";
+import Nav from "./components/Nav/Nav";
+import Model from "./pages/Model/Model";
+import Hero from "./pages/Hero/Hero";
+import Apply from "./pages/Apply/Apply";
+import Footer from "./pages/Footer/Footer";
+import TheWorldOfMaserati from "./pages/TheWorldOfMaserati/TheWorldOfMaserati";
 
 export class Main extends Component {
-
-    render() {
-        return (
-            <div>
-                <Nav />
-                <Hero />
-                <Model />
-                <TheWorldOfMaserati />
-                <Apply />
-                <Footer />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Nav />
+        <Hero />
+        <Model />
+        <TheWorldOfMaserati />
+        <Apply />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default Main;
--- src/pages/Model/ModelCont.js
+++ src/pages/Model/ModelCont.js
@@ -1,65 +1,77 @@
-import React, { Component } from 'react';
-import Ghibli from '../../images/Ghibli.jpeg';
-import Levante from '../../images/Levante.jpeg';
-import Quattroporte from '../../images/Quattroporte.jpeg';
+import React, { Component } from "react";
+import Ghibli from "../../images/Ghibli.jpeg";
+import Levante from "../../images/Levante.jpeg";
+import Quattroporte from "../../images/Quattroporte.jpeg";
 
 export class ModelCont extends Component {
-    render() {
-        return (
-            <>
-                <div className="ModelCont">
-                    <h2>Ghibli</h2>
-                    <div className="Medium">
-                        <p>YOU'RE NOT LIKE EVERYONE ELSE</p>
-                    </div>
+  render() {
+    return (
+      <>
+        <div className="ModelCont">
+          <h2>Ghibli</h2>
+          <div className="Medium">
+            <p>YOU'RE NOT LIKE EVERYONE ELSE</p>
+          </div>
 
-                    <div className="Bottom">
-                        <p>스타일, 파워, 스포티한 핸들링, 컴포트의 완벽한 조화</p>
-                    </div>
-                    <a className="ImgCt">
-                        <img src={Ghibli} />
-                    </a>
-                    <div className="BtnWrapper">
-                        <div className="CtWhite"><a href="#">자세히 보기</a></div>
-                        <div className="CtBlue"><a href="#">내 차량 만들기</a></div>
-                    </div>
-                </div>
-                <div className="ModelCont">
-                    <h2>Levante</h2>
-                    <div className="Medium">
-                        <p>THE MASERATI OF SUVs</p>
-                    </div>
-                    <div className="Bottom">
-                        <p>강력한 온로드-오프로드 성능</p>
-                    </div>
-                    <a className="ImgCt">
-                        <img src={Levante} />
-                    </a>
+          <div className="Bottom">
+            <p>스타일, 파워, 스포티한 핸들링, 컴포트의 완벽한 조화</p>
+          </div>
+          <a className="ImgCt">
+            <img src={Ghibli} />
+          </a>
+          <div className="BtnWrapper">
+            <div className="CtWhite">
+              <a href="#">자세히 보기</a>
+            </div>
+            <div className="CtBlue">
+              <a href="#">내 차량 만들기</a>
+            </div>
+          </div>
+        </div>
+        <div className="ModelCont">
+          <h2>Levante</h2>
+          <div className="Medium">
+            <p>THE MASERATI OF SUVs</p>
+          </div>
+          <div className="Bottom">
+            <p>강력한 온로드-오프로드 성능</p>
+          </div>
+          <a className="ImgCt">
+            <img src={Levante} />
+          </a>
 
-                    <div className="BtnWrapper">
-                        <div className="CtWhite"><a href="#">자세히 보기</a></div>
-                        <div className="CtBlue"><a href="#">내 차량 만들기</a></div>
-                    </div>
-                </div>
-                <div className="ModelCont">
-                    <h2>Quattroporte</h2>
-                    <div className="Medium">
-                        <p>AN ICON OF ITALIAN ELEGANCE</p>
-                    </div>
-                    <div className="Bottom">
-                        <p>세련된 퍼포먼스와 럭셔리함의 매혹적인 만남</p>
-                    </div>
-                    <a className="ImgCt">
-                        <img src={Quattroporte} />
-                    </a>
-                    <div className="BtnWrapper">
-                        <div className="CtWhite"><a href="#">자세히 보기</a></div>
-                        <div className="CtBlue"><a href="#">내 차량 만들기</a></div>
-                    </div>
-                </div>
-            </>
-        )
-    }
+          <div className="BtnWrapper">
+            <div className="CtWhite">
+              <a href="#">자세히 보기</a>
+            </div>
+            <div className="CtBlue">
+              <a href="#">내 차량 만들기</a>
+            </div>
+          </div>
+        </div>
+        <div className="ModelCont">
+          <h2>Quattroporte</h2>
+          <div className="Medium">
+            <p>AN ICON OF ITALIAN ELEGANCE</p>
+          </div>
+          <div className="Bottom">
+            <p>세련된 퍼포먼스와 럭셔리함의 매혹적인 만남</p>
+          </div>
+          <a className="ImgCt">
+            <img src={Quattroporte} />
+          </a>
+          <div className="BtnWrapper">
+            <div className="CtWhite">
+              <a href="#">자세히 보기</a>
+            </div>
+            <div className="CtBlue">
+              <a href="#">내 차량 만들기</a>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default ModelCont;
