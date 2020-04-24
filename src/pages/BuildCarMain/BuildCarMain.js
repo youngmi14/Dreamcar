46e32e3af1e2f8a7e255cccf055c9a0cdb18727b
Sun ju Lee <yohekisa@gmail.com>
2020-04-24 11:27:58 +0900

    in local master

--- src/pages/BuildCarMain/BuildCarMain.js
+++ src/pages/BuildCarMain/BuildCarMain.js
@@ -1,88 +1,65 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import CarSection from "./CarSection";
+import CarSummary from "./CarSummary";
+import Nav from "../../components/Nav/Nav";
 import "./BuildCarMain.scss";
-import CarSection from './CarSection';
-import CarSummary from './CarSummary';
-import Nav from '../../components/Nav/Nav';
-
-class TabItems extends Component {
-
-    constructor(props){
-        super(props);
-    }
-
-    render() {
-
-        const { hrefLink,className,onClick,onMouseOut,tabName } = this.props;
-
-        return(
-            <a href={hrefLink} className={this.props.tabName === this.props.isClickedName ? className : ""} onClick={onClick} onMouseOut={onMouseOut} name={tabName}>{tabName}</a>
-        );
-    }
-}
 
 class BuildCarMain extends Component {
+  state = {
+    hrefLink: "#carDisplay2",
+    tabId: "",
+  };
 
-    constructor(props){
-        super(props);
-    }
+  navMoveHandler = (id) => {
+    this.setState({
+      tabId: id,
+    });
+  };
 
-    state = {
-        isNavClicked: "",
-        hrefLink: "#carDisplay1",
-        tabNameList: ["외관", "실내", "패키지", "옵션", "액세서리", "요약"],
-        tabName: ""
-    }
+  mouseLeaveHandler = (e) => {
+    e.preventDefault();
 
-    navMoveHandler = (e) => {
+    this.setState({
+      isNavClickedClassName: "",
+      tabId: this.state.tabId,
+    });
+  };
 
-            console.log(e.target.name);
+  render() {
+    const tabNameList = ["외관", "실내", "패키지", "옵션", "액세서리", "요약"];
 
-            this.setState({
-                tabName: e.target.name,
-                isNavClicked: "clicked"
-            });
-    }
+    return (
+      <div className="BuildCarMain">
+        <Nav />
 
-    mouseLeaveHandler = (e) => {
+        <section className="BuildCarContent">
+          <nav className="topNav">
+            <ul className="tabList">
+              <li name="tabId" onMouseOut={this.mouseLeaveHandler}>
+                {tabNameList.map((item, idx) => {
+                  return (
+                    <a
+                      href={this.state.hrefLink}
+                      className={this.state.tabId === idx ? "clicked" : ""}
+                      onClick={() => this.navMoveHandler(idx)}
+                      onMouseOut={this.mouseLeaveHandler}
+                    >
+                      {item}
+                    </a>
+                  );
+                })}
+              </li>
+            </ul>
+          </nav>
 
-        e.preventDefault();
-
-        this.setState({
-            isNavClicked: "",
-            tabName: this.state.tabName
-        });
-    }
-
-    render() {
-
-        const {tabNameList} = this.state;
-
-        return (
-            <div className="BuildCarMain">
-                <Nav />
-
-                <section className="BuildCarContent">
-                <nav className="topNav">
-                    <ul className="tabList">
-                        <li name="tabId" onMouseOut={this.mouseLeaveHandler}>
-                            {tabNameList.map(item => {
-                                return(
-                                    <TabItems hrefLink={this.state.hrefLink} isClickedName={this.state.tabName} name={item} className={this.state.isNavClicked} tabName={item} onClick={this.navMoveHandler}/>
-                                )
-                            })}
-                        </li>
-                    </ul>
-                </nav>
-
-                <section className="mainView">
-                     <CarSection />
-                     <CarSummary />
-                </section>
-                </section>
-
-            </div>
-        )
-    }
+          <section className="mainView">
+            <CarSection />
+            <CarSummary />
+          </section>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default BuildCarMain;
